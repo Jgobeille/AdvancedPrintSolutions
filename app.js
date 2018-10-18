668bb9025db9a9d0f681cdaba635e883d3c5d06a
Jamie Gobeille <djent34@gmail.com>
2018-10-18 23:14:26 +0000

    Started beginnings of front page. Made navbar, mission statement, & featured products section

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+
+const express    = require("express");
+const app        = express();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var session    = require('express-session');
+var flash = require('connect-flash');
+
+
+//EJS view engine middleware
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+//express-session middleware
+app.use(session({
+  secret: 'Bella',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+
+//Express session Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//flash messages
+app.use(flash());
+
+
+//Routing for pages
+app.get("/", function(req,res) {
+    res.render("home");
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+console.log("The app has started!");
+});
